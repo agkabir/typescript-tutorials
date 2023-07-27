31d6349963f4a1560cd99ccff9eeb6717ed9629c
mkabir <ag.kabir@gmail.com>
2023-07-27 10:34:08 +0200

    assertion or typecasting in TS

---
+++ LESSION05/build/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+// convert to more or less specific
+let a = "hello";
+let b = a; // less specific
+let c = a; // more specific
+// angle bracket representation
+let d = 'world';
+let e = 'world';
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b;
+};
+let myVal = addOrConcat(2, 2, 'concat');
+// Obs ! TS sees no problem here but a string is returned
+let nextVal = addOrConcat(2, 2, "concat");
